f6c8bd5ec6aff64dd279fe17895df86521b26514
dongkeuny50 <dongkeuny50@gmail.com>
2021-05-21 01:21:05 +0900

    all

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as frontImage from './myface.jpeg';
 import { Box, } from '@material-ui/core';
-
 
 import Header from './Header';
 function App() {
+  
+  const logodiv = {
+    display: "flex",
+    width: 400,
+    height: 400,
+    marginLeft: 400,
+    borderRadius: 200,
+    borderColor: "black",
+    border: "1px solid #555",
+    backgroundColor: "grey",
+    justifyContent: "center",
+}
+  const img = frontImage.default;
   return (
     <div className="App">
       <Header />
       <main className="App-main">
         <Box height="60px" />
-        <div style={{ height: 100 }} >
-          <></>
+        <div style={{display: "flex"}}>
+          <div style={logodiv}>
+            <img style={{ height: 400,borderRadius: 200,}} src={img} alt="Test"></img>
+          </div>
+          <div>
+            <div style={{ height: 100}}></div>
+            <div style={{ height: 100}}></div>
+            <div style={{ height: 100}}></div>
+            <div style={{ height: 100}}></div>
+          </div>
         </div>
         <Box height="60px" />
       </main>
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,33 +1,23 @@
 import React from 'react';
 import './App.css';
 import { Box, Grid, } from '@material-ui/core';
-import styled from "styled-components";
-import Paper from '@material-ui/core/Paper';
 import * as logo from './ant.png';
+import { Navbar } from 'react-bootstrap'; 
+import './Header.css';
 function Header() {
-    const styles = {
-        header: {
-            height: "12vh",
-            background: "#c8c8c8",
-        },
-        footer: {
-            height: 100,
-            background: "#fff",
-        },
-    }
+    
     const logodiv = {
         display: "flex",
         width: 40,
         height: 40,
+        marginLeft: 25,
         borderRadius: 50,
         borderColor: "black",
         border: "1px solid #555",
         backgroundColor: "grey",
         justifyContent: "center"
     }
-    const img = logo.default;
-    return (
-        <header style={styles.header}>
+    /*
             <Grid container spacing={3}>
                 <Grid item xs={12} sm={6}>
                     <Box height="16px" />
@@ -46,8 +36,36 @@ function Header() {
                 <Grid item xs={12} sm={6}>
                     <Paper >xs=12 sm=6</Paper>
                 </Grid>
-            </Grid>
-        </header>
+            </Grid>*/
+    const img = logo.default;
+    const divider =  (
+        <><Box width="20px"/>
+        <Box>|</Box>
+        <Box width="20px"/></>
+    )
+
+    return (
+
+        <Navbar className = "head">
+            <Navbar.Brand>  
+                <div style={{display: 'flex'}}>
+                    <div style={logodiv}>
+                        <img style={{ height: 30}} src={img} alt="Test"></img>
+                    </div>
+                    <div style={{ marginTop: 3, marginLeft: 16, fontSize: "25px", fontWeight: 700, fontFamily: "'fRoboto Condensed',  sans-serif" }} >YOO DONGKEUN</div>
+                    <Box width="30px" />
+                    <div style={{ marginTop: 9, fontSize: "18px", fontFamily: "'fRoboto Condensed',  sans-serif" }}>Developer</div>
+                
+                    <div style={{ display: 'flex', position: 'absolute',right: 40}}>
+                    <div style={{ marginTop: 5,fontWeight: 700, fontFamily: "'fRoboto Condensed',  sans-serif" }}>About</div>
+                        {divider}
+                    <div style={{ marginTop: 5,fontWeight: 700, fontFamily: "'fRoboto Condensed',  sans-serif" }}>Work</div>
+                        {divider}
+                    <div style={{ marginTop: 5,fontWeight: 700, fontFamily: "'fRoboto Condensed',  sans-serif" }}>Contact</div>
+                    </div>
+                </div>
+            </Navbar.Brand>
+        </Navbar>
     );
 }
 
